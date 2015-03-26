c0d2974e4224494854e84d337277e3f61d8b5b13
Esteban Roncancio <eronanc@brown.edu>
2015-03-25 20:29:44 -0400

    added spark server for /

--- src/main/java/edu/brown/cs/dixit/Main.java
+++ src/main/java/edu/brown/cs/dixit/Main.java
@@ -1,9 +1,20 @@
 package edu.brown.cs.dixit;
 
+import joptsimple.OptionParser;
+import joptsimple.OptionSet;
+import joptsimple.OptionSpec;
+
 public class Main {
 
-	public static void main(String[] args) {
-		System.out.println("Welcome to Dixit.");
-	}
-
+  public static void main(String[] args) {
+    OptionParser parser = new OptionParser();
+    OptionSpec<Integer> port = parser.accepts("port").withRequiredArg()
+        .ofType(Integer.class);
+    try {
+      OptionSet options = parser.parse(args);
+      Dixit.runSparkSever(port.value(options));
+    } catch (RuntimeException e) {
+      System.err.println("ERROR: " + e.getMessage());
+    }
+  }
 }
---
+++ src/main/resources/spark/template/freemarker/dixit.ftl
@@ -0,0 +1,5 @@
+<#assign content>
+	<div class="container">
+	</div>
+</#assign>
+<#include "main.ftl">
