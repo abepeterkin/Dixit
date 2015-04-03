d59ba8381d2bc16721ae60f7aa898b20dccb7f4b
Esteban Roncancio <eroncanc@cs.brown.edu>
2015-04-03 14:53:12 -0400

    testing eclipse git integration

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -22,6 +22,7 @@ window.onload = function(){
   	clientPlayer = new Player(1, "#000000", false, game);
   	clientPlayer.addHand(makeTestHand());
   	drawBoard();
+  	//test
   }
   
   function makeTestHand(){
