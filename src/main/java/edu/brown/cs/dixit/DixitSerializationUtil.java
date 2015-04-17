cf2a48f6cb9b741ad4ad1268c334e3937f8c6a9a
Esteban Roncancio <eroncanc@cs.brown.edu>
2015-04-17 02:06:08 -0400

    removed colors I dont have sprites for

--- src/main/java/edu/brown/cs/dixit/DixitSerializationUtil.java
+++ src/main/java/edu/brown/cs/dixit/DixitSerializationUtil.java
@@ -15,25 +15,21 @@ import com.google.common.collect.ImmutableMap;
 import com.google.gson.Gson;
 
 /**
- *
  * Serializes various classes into strings. Deep serialization means that nested
  * objects are recursively serialized.
- *
  */
 public class DixitSerializationUtil {
 
   private final static Gson GSON = new Gson();
 
-  public String serializeCard(
-      Card card) {
+  public String serializeCard(Card card) {
     Map<String, Object> variables = new ImmutableMap.Builder()
         .put("id", card.getId()).put("image", card.getImage())
         .put("isStoryTeller", card.getStoryteller()).build();
     return GSON.toJson(variables);
   }
 
-  public String serializeHand(
-      List<Card> hand) {
+  public String serializeHand(List<Card> hand) {
     ImmutableList.Builder tempBuilder = new ImmutableList.Builder();
     int index = 0;
     while (index < hand.size()) {
@@ -44,23 +40,18 @@ public class DixitSerializationUtil {
     return GSON.toJson(tempBuilder.build());
   }
 
-  public String serializeColor(
-      Color color) {
+  public String serializeColor(Color color) {
     String tempValue = "none";
     if (color == Color.RED) {
       tempValue = "RED";
-    } else if (color == Color.ORANGE) {
-      tempValue = "ORANGE";
+    } else if (color == Color.WHITE) {
+      tempValue = "WHITE";
     } else if (color == Color.YELLOW) {
       tempValue = "YELLOW";
     } else if (color == Color.GREEN) {
       tempValue = "GREEN";
-    } else if (color == Color.CYAN) {
-      tempValue = "CYAN";
     } else if (color == Color.BLUE) {
       tempValue = "BLUE";
-    } else if (color == Color.VIOLET) {
-      tempValue = "VIOLET";
     } else if (color == Color.PINK) {
       tempValue = "PINK";
     }
@@ -69,25 +60,20 @@ public class DixitSerializationUtil {
     return GSON.toJson(variables);
   }
 
-  public String serializePlayer(
-      Player player,
-      Player currentPlayer) {
+  public String serializePlayer(Player player, Player currentPlayer) {
     Map<String, Object> variables = new ImmutableMap.Builder()
         .put("score", player.getScore()).put("chatName", player.getChatName())
         .put("isStoryTeller", player.isStoryteller()).build();
     return GSON.toJson(variables);
   }
 
-  public String deepSerializePlayer(
-      Player player,
-      Player currentPlayer) {
+  public String deepSerializePlayer(Player player, Player currentPlayer) {
     // TODO: Serialize.
     // Cannot serialize yet because player has no accessor for hand.
     return "";
   }
 
-  public String serializePhase(
-      Phase phase) {
+  public String serializePhase(Phase phase) {
     String tempValue = "none";
     if (phase == Phase.STORYTELLER) {
       tempValue = "STORYTELLER";
@@ -105,24 +91,19 @@ public class DixitSerializationUtil {
     return GSON.toJson(variables);
   }
 
-  public String serializeGame(
-      Game game,
-      Player currentPlayer) {
+  public String serializeGame(Game game, Player currentPlayer) {
     // TODO: Serialize.
     // Need accessors for serialization.
     return "";
   }
 
-  public String deepSerializeGame(
-      Game Game,
-      Player currentPlayer) {
+  public String deepSerializeGame(Game Game, Player currentPlayer) {
     // TODO: Serialize.
     // Need accessors for serialization.
     return "";
   }
 
-  public String serializeChatLine(
-      ChatLine chatLine) {
+  public String serializeChatLine(ChatLine chatLine) {
     return GSON.toJson(chatLine.getMessage());
   }
 
