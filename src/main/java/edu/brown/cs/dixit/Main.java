f097e9b8e6255581e8cccb704fcaf81d1dffed2a
John Eisenmann <esperantanaso@gmail.com>
2015-04-28 14:30:48 -0400

    Synchronization of methods.

--- src/main/java/edu/brown/cs/dixit/Main.java
+++ src/main/java/edu/brown/cs/dixit/Main.java
@@ -7,11 +7,11 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import com.google.gson.Gson;
-
 import joptsimple.OptionParser;
 import joptsimple.OptionSet;
 import joptsimple.OptionSpec;
+
+import com.google.gson.Gson;
 
 public class Main {
 
@@ -32,44 +32,45 @@ public class Main {
     }
   }
 
-  /** Returns the next id to be assigned to a player, and increments the value
-   * so that the next player will get a new id.
+  /**
+   * Returns the next id to be assigned to a player, and increments the value so
+   * that the next player will get a new id.
    *
    * @return an id for a new player
    */
-  public static String newId() {
+  synchronized public static String newId() {
     String toReturn = Integer.toString(nextId);
     nextId++;
     return toReturn;
   }
 
-  public static boolean gameExists(
+  synchronized public static boolean gameExists(
       String name) {
-    //System.out.println(gameMap.keySet());
+    // System.out.println(gameMap.keySet());
     return gameMap.containsKey(name);
   }
 
-  public static void addGame(
+  synchronized public static void addGame(
       String name,
       Game game) {
     gameMap.put(name, game);
   }
 
-  public static Game getGame(
+  synchronized public static Game getGame(
       String name) {
     return gameMap.get(name);
   }
 
-  public static void removeGame(
+  synchronized public static void removeGame(
       String name) {
     gameMap.remove(name);
   }
 
-  public static List<String> getGameNameList() {
+  synchronized public static List<String> getGameNameList() {
     return new ArrayList<String>(gameMap.keySet());
   }
 
-  public static List<Game> getGameList() {
+  synchronized public static List<Game> getGameList() {
     return new ArrayList<Game>(gameMap.values());
   }
 
